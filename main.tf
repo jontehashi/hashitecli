c4631712877a3cc62e55053b977003e40c596c9c
Jon Hefner <jontehashi@gmail.com>
2024-07-14 13:44:56 -0400

    deleted some resources

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "jon_windows_box" {
 
 resource "aws_security_group" "allows_linux_traffic" {
   name        = "allow_traffic"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow all inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.main.id
 
   tags = {
@@ -46,5 +46,5 @@ resource "aws_vpc_security_group_ingress_rule" "llow_all_traffic_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allows_linux_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" 
 }
