c7c09db6dbd0a9b2acdf70ecb9c4dd5c33f525ab
Jon Hefner <jontehashi@gmail.com>
2024-07-14 12:58:22 -0400

    adding a second instances

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 
-resource "aws_instance" "Jon_linux_box" {
+resource "aws_instance" "jon_linux_box" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Jon's Ec2"
+    Name = "Jon's linux"
+  }
+}
+
+resource "aws_instance" "jon_windows_box" {
+  ami           = "ami-07d9456e59793a7d5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jon's windows"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.Jon_linux_box.id
+output "jon_linux_id" {
+  description = "The ID of the linux EC2 instance"
+  value       = aws_instance.jon_linux_box.id
+}
+
+output "jon_windows_id" {
+  description = "The ID of the windows instance"
+  value       = aws_instance.jon_windows_box.id
 }
