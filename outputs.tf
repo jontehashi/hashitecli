5d767f420c3c7b69024edfc5f5b49c6d30e794ae
Jon Hefner <jontehashi@gmail.com>
2024-07-14 23:08:16 -0400

    add outputs back in

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-#output "imported_vpc_id" {
-#  description = "The ID of the windows instance"
-#  value       = aws_vpc.main.id
-#}
-#
-#output "jon_linux_id" {
-#  description = "The ID of the linux EC2 instance"
-#  value       = aws_instance.jon_linux_box.id
-#}
-#
-#output "jon_windows_id" {
-#  description = "The ID of the windows instance"
-#  value       = aws_instance.jon_windows_box.id
-#}
+output "imported_vpc_id" {
+  description = "The ID of the windows instance"
+  value       = aws_vpc.main.id
+}
+
+output "jon_linux_id" {
+  description = "The ID of the linux EC2 instance"
+  value       = aws_instance.jon_linux_box.id
+}
+
+output "jon_windows_id" {
+  description = "The ID of the windows instance"
+  value       = aws_instance.jon_windows_box.id
+}
