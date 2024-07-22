9534520ad8c49a2eeef2430f8764fdf00660e813
Jon Hefner <jontehashi@gmail.com>
2024-07-22 19:57:25 -0400

    updated security group rules

--- main.tf
+++ main.tf
@@ -38,13 +38,13 @@ resource "aws_security_group" "allows_linux_traffic_cli" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allllow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allows_linux_traffic.id
+  security_group_id = aws_security_group.allows_linux_traffic_cli
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allows_linux_traffic.id
+  security_group_id = aws_security_group.allows_linux_traffic_cli
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
 }
