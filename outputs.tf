a9b051bbe8e6c3446686d0aab39c1e61387db346
Jon Hefner <jontehashi@gmail.com>
2024-07-22 19:56:04 -0400

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "imported_vpc_id" {
+output "vpc_id" {
   description = "The ID of the windows instance"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.maincli
 }
 
 output "jon_linux_id" {
   description = "The ID of the linux EC2 instance"
-  value       = aws_instance.jon_linux_box.id
+  value       = aws_instance.jon_linux_box_cli.id
 }
 
 output "jon_windows_id" {
   description = "The ID of the windows instance"
-  value       = aws_instance.jon_windows_box.id
+  value       = aws_instance.jon_windows_box_cli
 }
