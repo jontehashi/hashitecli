8003097d8af70210423689b3e3774751a52b1885
Jon Hefner <jontehashi@gmail.com>
2024-07-22 19:54:40 -0400

    update names

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "maincli" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_instance" "jon_linux_box" {
+resource "aws_instance" "jon_linux_box_cli" {
   ami           = "ami-04a81a99f5ec58529" #free tier linux ami
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.allows_linux_traffic.id ]
@@ -17,7 +17,7 @@ resource "aws_instance" "jon_linux_box" {
   }
 }
 
-resource "aws_instance" "jon_windows_box" {
+resource "aws_instance" "jon_windows_box_cli" {
   ami           = "ami-07d9456e59793a7d5" #free tier windows ami
   instance_type = "t2.micro"
 
@@ -26,10 +26,10 @@ resource "aws_instance" "jon_windows_box" {
   }
 }
 
-resource "aws_security_group" "allows_linux_traffic" {
+resource "aws_security_group" "allows_linux_traffic_cli" {
   name        = "allow_traffic"
   description = "Allow all inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.maincli.id
 
   tags = {
     Name = "allow_traffic"
@@ -37,7 +37,7 @@ resource "aws_security_group" "allows_linux_traffic" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "llow_all_traffic_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allllow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allows_linux_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
